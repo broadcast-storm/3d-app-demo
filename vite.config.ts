2f685db093b8c9ddcb6a03065870afc40f0ef1cc
Nikita Pozdnyakov <nikita220800@mail.ru>
2023-12-14 19:48:35 +0300

    solid devtools added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-    plugins: [solid()],
-    base: '3d-app-demo',
+    plugins: [
+        devtools({
+            autoname: true,
+            locator: {
+                key: 'Control',
+                targetIDE: 'vscode',
+                componentLocation: true,
+                jsxLocation: true,
+            },
+        }),
+        solid(),
+    ],
+    base: '/3d-app-demo',
 });
