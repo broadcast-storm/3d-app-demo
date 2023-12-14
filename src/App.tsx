70a7b9ac8e8acc9a315f165ee8b8c1d6afee3957
Nikita Pozdnyakov <nikita220800@mail.ru>
2023-12-14 15:45:27 +0300

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { createSignal, onMount, Ref } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { setupCubeDemo } from './components/cube.demo';
 
 let canvasRef: Ref<HTMLCanvasElement> | undefined;
@@ -16,23 +13,15 @@ function App() {
   })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-      </div>
+    <div class='flex flex-col justify-center items-center gap-8 p-4 bg-slate-100 h-screen'>
+    <h1 class="text-3xl font-bold">
+      Hello world!
+    </h1>
+      <button class='bg-blue-500 text-cyan-50 py-2 px-4 block rounded-md' onClick={() => setCount((count) => count + 1)}>
+        count is {count()}
+      </button>
       <canvas ref={canvasRef} style={{width: '600px', height: '400px'}} />
-    </>
+    </div>
   )
 }
 
