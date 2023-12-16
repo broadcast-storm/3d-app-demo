45aabf1711a2bbe060b239cc04d3c314277bd4d9
Nikita Pozdnyakov <nikita220800@mail.ru>
2023-12-16 18:20:27 +0300

    added hash router for github pages

--- src/app/Application.tsx
+++ src/app/Application.tsx
@@ -1,16 +1,28 @@
 import '@/shared/styles/base.css';
 
-import { Router } from '@solidjs/router';
+import { HashRouter, Router } from '@solidjs/router';
+import { ErrorBoundary, Match, Switch } from 'solid-js';
 
-import { ENV_CONFIG } from '@/shared/config/environment';
+import { ENV_CONFIG, isHashRouter } from '@/shared/config/environment';
 import { PageLayout } from '@/shared/ui/page-layout';
 
 import { Routes } from './Router';
 
 export function Application() {
     return (
-        <Router base={ENV_CONFIG.BASE_URL} root={PageLayout}>
-            {Routes}
-        </Router>
+        <ErrorBoundary fallback={<div>Something went terribly wrong</div>}>
+            <Switch fallback={<div>Not Found</div>}>
+                <Match when={isHashRouter}>
+                    <HashRouter base={ENV_CONFIG.BASE_URL} root={PageLayout}>
+                        {Routes}
+                    </HashRouter>
+                </Match>
+                <Match when={!isHashRouter}>
+                    <Router base={ENV_CONFIG.BASE_URL} root={PageLayout}>
+                        {Routes}
+                    </Router>
+                </Match>
+            </Switch>
+        </ErrorBoundary>
     );
 }
