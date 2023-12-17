978ea22cd898ece78e50c2f6371fbe7ad22c53ef
Nikita Pozdnyakov <nikita220800@mail.ru>
2023-12-17 14:27:51 +0300

    added preload pages

--- src/app/Application.tsx
+++ src/app/Application.tsx
@@ -1,7 +1,7 @@
 import '@/shared/styles/base.css';
 
 import { HashRouter, Router } from '@solidjs/router';
-import { ErrorBoundary, Match, Switch } from 'solid-js';
+import { ErrorBoundary } from 'solid-js';
 
 import { ENV_CONFIG, isHashRouter } from '@/shared/config/environment';
 import { PageLayout } from '@/shared/ui/page-layout';
@@ -9,18 +9,15 @@ import { PageLayout } from '@/shared/ui/page-layout';
 import { Routes } from './Router';
 
 export function Application() {
+    const CurrentRouter = isHashRouter ? HashRouter : Router;
     return (
         <ErrorBoundary fallback={<div>Something went terribly wrong</div>}>
-            <Switch fallback={<div>Not Found</div>}>
-                <Match when={isHashRouter}>
-                    <HashRouter root={PageLayout}>{Routes}</HashRouter>
-                </Match>
-                <Match when={!isHashRouter}>
-                    <Router base={ENV_CONFIG.BASE_URL} root={PageLayout}>
-                        {Routes}
-                    </Router>
-                </Match>
-            </Switch>
+            <CurrentRouter
+                preload={true}
+                root={PageLayout}
+                base={!isHashRouter ? ENV_CONFIG.BASE_URL : undefined}>
+                {Routes}
+            </CurrentRouter>
         </ErrorBoundary>
     );
 }
