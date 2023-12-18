545b1cdd6e4ffbdf37019be5ce3bb6d6e37f8a6d
Nikita Pozdnyakov <nikita220800@mail.ru>
2023-12-18 21:12:27 +0300

    tw-merge added

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,6 +1,8 @@
 import { Motion } from '@motionone/solid';
 import type { Component, ComponentProps } from 'solid-js';
 
+import { cn } from '@/shared/utils/styles';
+
 type Props = ComponentProps<'button'> & {
     label: string;
     size?: 'large' | 'small';
@@ -10,8 +12,11 @@ export const Button: Component<Props> = (props) => {
     return (
         <Motion.button
             hover={{ scale: 1.05 }}
-            press={{ scale: 0.95 }}
-            class="px-4 py-2 rounded-md bg-slate-950 text-white">
+            press={{ scale: 0.9 }}
+            class={cn(
+                'px-4 py-2 rounded-md bg-slate-950 text-white',
+                props.class,
+            )}>
             {props.label}
         </Motion.button>
     );
